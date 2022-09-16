71b1d3a55cb7373ebe620d4f95db7935b93511d2
Jcapponi <julicapponi@hotmail.com>
2022-09-16 00:21:58 -0300

    CONFIG DATABASE HEROKU

--- src/database/database.js
+++ src/database/database.js
@@ -2,11 +2,19 @@ import mysql from "promise-mysql";
 import config from "./../config.js";
 
 const connection = mysql.createConnection({
-    host: config.host,
-    database: config.database,
-    user: config.user,
-    password: config.password
+    /*
+    host: config.host, 
+    database: config.database, 
+    user: config.user, 
+    password: config.password 
+*/
+    //BD HEROKU
+    host: 'us-cdbr-east-06.cleardb.net',
+    database: 'heroku_1e935464b510891', 
+    user: 'b97f768dd64fe5', 
+    password: '6b2f98f2' 
 });
+
 
 export const getConnection = () => {
     return connection;
