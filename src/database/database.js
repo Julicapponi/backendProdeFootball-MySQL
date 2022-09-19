21abd0d9b0ffd3d99a8a8fe6b30f814ea7804f31
Jcapponi <julicapponi@hotmail.com>
2022-09-18 22:51:40 -0300

    config

--- src/database/database.js
+++ src/database/database.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
 */
     //BD HEROKU
     host: 'us-cdbr-east-06.cleardb.net',
-    database: 'heroku_9da121ea22b70de', 
+    database: 'heroku_9da121ea22b70de',  
     user: 'b006be620c9751', 
     password: 'eb26edaf' 
 });
