9c01f286bcc9fb4deb428e2a5141cfe444d36c79
Jcapponi <julicapponi@hotmail.com>
2022-09-19 20:22:12 -0300

    pool connection

--- src/database/database.js
+++ src/database/database.js
@@ -1,24 +1,29 @@
 import mysql from "promise-mysql";
+import createPool from "mysql";
 import config from "./../config.js";
 
-const connection = mysql.createConnection({
-    /*
-    host: config.host, 
-    database: config.database, 
-    user: config.user, 
-    password: config.password 
-*/
-    //BD HEROKU
-    host: 'us-cdbr-east-06.cleardb.net',
-    database: 'heroku_9da121ea22b70de',  
-    user: 'b006be620c9751', 
-    password: 'eb26edaf' 
-});
+    const connection = mysql.createPool({
+        
+        /*
+        host: config.host, 
+        database: config.database, 
+        user: config.user, 
+        password: config.password 
+    */
+        //BD HEROKU
+        connectionLimit: 10,
+        host: 'us-cdbr-east-06.cleardb.net',
+        database: 'heroku_9da121ea22b70de',  
+        user: 'b006be620c9751', 
+        password: 'eb26edaf' 
+    });
 
 
 
 export const getConnection = () => {
     return connection;
 };
+
+
 
 
