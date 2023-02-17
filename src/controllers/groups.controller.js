7208a175e74e7c948bed8ec7c8cd7e1b30da53a2
Jcapponi <julicapponi@hotmail.com>
2023-02-17 18:33:29 -0300

    modificacion en grupos por postulaciones de usuarios

--- src/controllers/groups.controller.js
+++ src/controllers/groups.controller.js
@@ -2,6 +2,7 @@ import { getConnection } from "../database/database.js";
 import * as bcryptjs from 'bcryptjs';
 
 
+
 const addGroup = async (req, res) => {
     try {
         const { nameGrupo, idUserGreaGrupo } = req.body;
@@ -40,8 +41,26 @@ const addGroup = async (req, res) => {
     }
 };
 
+const addPostularGroup = async (req, res) => {
+    try {
+        const { userId, groupId } = req.body;
+        console.log("usuario con id:", userId);
+        console.log("grupo a Postularse:", groupId);
+        if (userId === undefined || groupId === undefined) {
+            return res.status(400).json({ message: "No se pudo postular el usuario al grupo" });
+        }
+        const connection = await getConnection();
+        const sqlInsertUserPostularGroup = `INSERT INTO postulaciones_grupo (user_id, group_id) VALUES('${userId}','${groupId}')`;
+        await connection.query(sqlInsertUserPostularGroup);
+        return res.status(200).json({message: "Usuario postulado al grupo con exito"});
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
 
-const addUserGroup = async (req, res) => {
+
+ async function addUserGroup (req, res) {
     try {
         const { userId, groupId } = req.body;
         console.log("usuario con id:", userId);
@@ -54,6 +73,75 @@ const addUserGroup = async (req, res) => {
         const sqlInsertUserGroup = `INSERT INTO miembros_grupo (user_id, group_id) VALUES('${userId}','${groupId}')`;
         await connection.query(sqlInsertUserGroup);
         return res.status(200).json({message: "Usuario agregado al grupo con exito"});
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+//eliminar postulacion
+async function deletePostulacion (req, res) {
+    try {
+        const { userId, groupId } = req.body;
+        console.log("usuario con id:", userId);
+        console.log("grupo a unir:", groupId);
+        if (userId === undefined || groupId === undefined) {
+            return res.status(400).json({ message: "No se pudo eliminar el usuario de la postulacion del grupo" });
+        }
+        const connection = await getConnection();
+        // elimina user de la postulacion del grupo
+        const sqlDeletePostulacion = `DELETE FROM postulaciones_grupo WHERE user_id='${userId}' AND group_id='${groupId}'`;
+        await connection.query(sqlDeletePostulacion);
+        return res.status(200).json({message: "Usuario eliminado de la postulacion con exito"});
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+const aceptarPostulante = async (req, res) => {
+    const connection = await getConnection();
+    await addUserGroup(req, res);
+    await deletePostulacion(req, res);
+
+};
+
+const rechazarPostulante = async (req, res) => {
+    const connection = await getConnection();
+    await deletePostulacion(req, res);
+};
+
+
+
+
+const listPostulaciones = async (req, res) => {
+    try {
+        const {idUser } = req.params;
+        console.log(idUser);
+        if (idUser === undefined) {
+            res.status(400).json({ message: "No se pudo obtener el user"});
+        }
+        const connection = await getConnection();
+        const sqlListUsersPostulaciones = `SELECT * FROM postulaciones_grupo WHERE user_id = '${idUser}'`;
+        const result = await connection.query(sqlListUsersPostulaciones);
+        return res.status(200).json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+const listPostulacionesPendientes = async (req, res) => {
+    try {
+        const {idUser } = req.params;
+        console.log(idUser);
+        if (idUser === undefined) {
+            res.status(400).json({ message: "No se pudo obtener el user"});
+        }
+        const connection = await getConnection();
+        const sqlListUsersPostulacionesPendientes = `SELECT * FROM grupos JOIN postulaciones_grupo ON grupos.idGrupo = postulaciones_grupo.group_id JOIN users ON users.iduser = postulaciones_grupo.user_id WHERE grupos.idUserCreador = '${idUser}'`;
+        const result = await connection.query(sqlListUsersPostulacionesPendientes);
+        return res.status(200).json(result);
     } catch (error) {
         res.status(500);
         res.send(error.message);
@@ -143,8 +231,14 @@ const deleteGroup = async (req, res) => {
 
 export const methods = {
     addGroup,
+    addPostularGroup,
     addUserGroup,
+    deletePostulacion,
+    aceptarPostulante,
+    rechazarPostulante,
     listUsersGroup,
+    listPostulaciones,
+    listPostulacionesPendientes,
     gruposPorUser,
     listGroupsBusqueda,
     deleteUserGroup,
