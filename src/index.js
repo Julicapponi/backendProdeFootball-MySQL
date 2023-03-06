78b4b0c02041b9b9c31049bb0b256f230ae1c6f8
Jcapponi <julicapponi@hotmail.com>
2023-03-05 22:17:44 -0300

    pruebas de tareas programadas con node-cron, listo para cuando tenga la sincronizacion de calculo de puntajes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import app from "./app.js";
+import cron from "node-cron";
+import {methods} from "./controllers/enfrentamientos.controller.js";
 
 const main = () => {
     app.listen(app.get("port"),app.get("host"));
     console.log(`Server on port ${app.get("port")} en el host ${app.get("host")}`);
+
+    cron.schedule("*/15 * * * * *", () => {
+        console.log("La tarea programada cada 15 segundos se ha ejecutado");
+        //methods.saveEnfrentamientosCompetenciasActivas();
+    });
+/*
+
+    cron.schedule("* * * * * *", () => {
+        console.log("La tarea programada cada 1 segundo se ha ejecutado");
+
+    });
+
+    cron.schedule('0 0 * * *', () => {
+        console.log("La tarea programada de las 00:00 horas se ha ejecutado");
+    });
+
+    cron.schedule('0 23 * * *', () => {
+        console.log("La tarea programada de las 23:00 horas se ha ejecutado");
+    });
+*/
+   
 };
 
 main();
