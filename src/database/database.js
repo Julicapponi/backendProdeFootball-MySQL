d0bdb5077eac9b3f7e7b8d33434e4948a6d62427
Jcapponi <julicapponi@hotmail.com>
2023-03-25 15:03:36 -0300

    database error

--- src/database/database.js
+++ src/database/database.js
@@ -2,9 +2,11 @@ import mysql from "promise-mysql";
 import config from "./../config.js";
 
 const connection = mysql.createPool({
-        
+    connectionLimit: 1000,
+    connectTimeout: 60 * 60 * 1000,
+    acquireTimeout: 60 * 60 * 1000,
+    timeout: 60 * 60 * 1000,
     multipleStatements: true,
-    connectionLimit: 100,
     host: config.host, 
     database: config.database, 
     user: config.user, 
