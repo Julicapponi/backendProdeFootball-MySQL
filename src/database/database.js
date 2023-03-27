ccd458409177331d0d7c86748ec8a7bb06c69c5c
Jcapponi <julicapponi@hotmail.com>
2023-03-27 00:17:18 -0300

    pruebas

--- src/database/database.js
+++ src/database/database.js
@@ -1,34 +1,33 @@
 import mysql from "mysql2/promise";
 import config from "./../config.js";
-export let connection;
+let pool;
 
-try {
-  connection = mysql.createPool({
-    
-    connectionLimit: 1000,
-    multipleStatements: true,
-    host: config.host, 
-    database: config.database, 
-    user: config.user, 
-    password: config.password,
-    rowsAsArray: false,
-    ssl: false,
-    acquireTimeout:6000000
-  });
+export const getConnection = async () => {
+  if (!pool) {
+    pool = mysql.createPool({
+      connectionLimit: 1000,
+      multipleStatements: true,
+      host: config.host,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      rowsAsArray: false,
+      ssl: false,
+      acquireTimeout: 6000000
+    });
+
+    pool.on('error', (err) => {
+      console.error('Database connection error:', err);
+      pool.end();
+    });
+  }
+
+  const connection = await pool.getConnection();
+
   connection.on('error', (err) => {
     console.error('Database connection error:', err);
-    connection.destroy();
+    connection.release();
   });
-  console.log("Connection to database successful.");
-} catch (error) {
-  console.error("Error connecting to database: ", error.message);
-}
 
-export const getConnection = () => {
-  if (!connection) {
-    console.error("No connection to database.");
-    console.log(connection.error);
-    console.log(connection);
-  }
   return connection;
 };
